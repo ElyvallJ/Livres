aabda7b9d4c4bee343464674a676f01ebb375487
Elyvall Jiddou <elyvall.jiddou@monboreal.ca>
2024-10-18 18:53:28 -0400

    Dernier version

--- AppConsoleLivres/Program.cs
+++ AppConsoleLivres/Program.cs
@@ -8,30 +8,37 @@ internal class Program
         Revue revue1 = new Revue("Monde", "Alba", 352, 45, 1980);
         Revue revue2 = new Revue("Ciel", "Messi", 456, 25, 1999);
         Revue revue3 = new Revue("Terre", "Xavi", 548, 5, 2006);
-
         // la création d'une liste et l'ajout des élèments dans la liste
         List<Revue> Revues = new List<Revue> { revue1, revue2, revue3 };
-
-
         Console.WriteLine("les revues sont  :");
         foreach (var Revue in Revues)
         {
             Revue.AfficherDetails();
         }
+        Console.WriteLine();
+
 
         // les instances de la classe Roman
         Roman roman1 = new Roman("l'enfant noir", "Camara Laye", 221, "Hisoire");
         Roman roman2 = new Roman("Maimouna", "Abdoulaye Sadji", 251, "Education");
         Roman roman3 = new Roman("soue l'orage", "Seydou Badian", 253, "Conflit");
-
         // la création d'une liste et l'ajout des élèments dans la liste
         List<Roman> Romans = new List<Roman> { roman1,roman2,roman3 };
-
-
         Console.WriteLine("les romans sont :");
         foreach (var Roman in Romans)
         {
             Roman.AfficherDetails();
+        }
+        Console.WriteLine();
+
+
+        //Une liste generique de livre 
+        List<Livre> livres = new List<Livre> { revue1, revue2, revue3, roman1, roman2, roman3 };
+        
+        Console.WriteLine("les Livres sont :");
+        foreach (var Livre in livres)
+        {
+            Livre.AfficherDetails();
         }
     }
 }
