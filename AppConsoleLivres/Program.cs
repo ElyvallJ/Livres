cea95f62f6579d66b670f9b67d55fe71ad52eb99
Elyvall Jiddou <elyvall.jiddou@monboreal.ca>
2024-10-18 18:28:00 -0400

    Troisieme version

--- AppConsoleLivres/Program.cs
+++ AppConsoleLivres/Program.cs
@@ -1,7 +1,22 @@
-﻿internal class Program
+﻿using AppConsoleLivres;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // les instances de la classe Revue
+        Revue revue1 = new Revue("Monde", "Alba", 352, 45, 1980);
+        Revue revue2 = new Revue("Ciel", "Messi", 456, 25, 1999);
+        Revue revue3 = new Revue("Terre", "Xavi", 548, 5, 2006);
+
+        // la création d'une liste et l'ajout des élèments dans la liste
+        List<Revue> Revues = new List<Revue> { revue1, revue2, revue3 };
+
+
+        Console.WriteLine("les livres dans la liste, leurs details est :");
+        foreach (var Revue in Revues)
+        {
+            Revue.AfficherDetails();
+        }
     }
 }
---
+++ AppConsoleLivres/Revue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConsoleLivres
+{
+    internal class Revue : Livre
+    {
+        // la propriete
+        public int Numero { get; set; }
+        public int Annee { get; set; }
+
+        // le constructeur
+        public Revue(string titre, string auteur, int nombrepages, int numero, int annee) : base(titre, auteur, nombrepages)
+        {
+            Numero = numero;
+            Annee = annee;
+        }
+
+        //la methode 
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($"le titre du revue est {Titre},l'auteur est {Auteur},le nomre de page est {NombrePages},le numero de revue est {Numero} et l'annee de sortie est {Annee}.");
+        }
+
+    }
+}
